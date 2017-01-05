71ff4f084160276d98a56999a7ad5580a2315e7a
dan100110 <daniel.auerbach@gmail.com>
2017-01-04 16:52:09 -0800

    attempt to switch to mongodb again.  This time inside of the docker container.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get -y install mongodb-org
+sudo service mongod start
+
+
 # Symbolic Link to the SuperGLU and GLUDB Repos
 cd ..
 sudo ln -s ./SuperGLURepo/SuperGLU/ ./Application/
